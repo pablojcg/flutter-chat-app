6f8ce516da356f34ee3e9968dd82c3a27a7c10f4
pablojcg <pablojcg@gmail.com>
2021-07-19 21:19:51 -0500

    Fin Sección 7 diseño de los chats

--- lib/src/pages/chat_page.dart
+++ lib/src/pages/chat_page.dart
@@ -1,14 +1,143 @@
+import 'dart:io';
+
+import 'package:chat_pc/src/widgets/chat_message_widget.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
+
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> with TickerProviderStateMixin{
+
+  final _textController = new TextEditingController();
+  final _focusNode = new FocusNode();
+  List<ChatMessageWidget> _messages = [];
+  bool _isTyping = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola ChatPage'),
-     ),
-   );
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Column(
+          children: [
+            CircleAvatar(
+              child: Text('PC', style: TextStyle(fontSize: 12),),
+              backgroundColor: Colors.blue[100],
+              maxRadius: 14.0,
+            ),
+            SizedBox(height: 3.0,), 
+            Text('Pablo Castro', style: TextStyle(color: Colors.black87, fontSize: 12.0),)
+          ],
+        ),
+        centerTitle: true,
+        elevation: 1.0,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Flexible(
+              child: ListView.builder(
+                physics: BouncingScrollPhysics(),
+                itemCount: _messages.length,
+                itemBuilder: (_, index) => _messages[index],
+                reverse: true,
+              )
+            ), 
+            Divider(height: 1.0,), 
+            // TODO Caja de texto
+            Container(
+              color: Colors.white,
+              child: _inputChat(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _inputChat(){
+
+    return SafeArea(
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 5.0),
+        child: Row(
+          children: [
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmit,
+                onChanged: (String texto){
+                  setState(() {
+                    if(texto.trim().length > 0) this._isTyping = true;
+                    else this._isTyping = false;
+                  });
+                },
+                decoration: InputDecoration.collapsed(
+                  hintText: 'Enviar mensaje'
+                ),
+                focusNode: _focusNode,
+              )
+            ),
+            //Boton Enviar
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: Platform.isIOS 
+              ? CupertinoButton(
+                child: Text('Enviar'), 
+                onPressed: (this._isTyping) 
+                    ? () => _handleSubmit(_textController.text.trim())
+                    : null ,
+              )
+              : Container(
+                margin: EdgeInsets.symmetric(horizontal: 4.0),
+                child: IconTheme(
+                  data: IconThemeData(color: Colors.blue[400]),
+                  child: IconButton(
+                    highlightColor: Colors.transparent,
+                    splashColor: Colors.transparent,
+                    icon: Icon(Icons.send,),
+                    onPressed: (this._isTyping) 
+                    ? () => _handleSubmit(_textController.text.trim())
+                    : null ,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _handleSubmit(String text){
+    if(text.length == 0) return;
+    print(text);
+    final newMessage = new ChatMessageWidget(
+      text: text, 
+      uid: '123',
+      animationController: AnimationController(vsync: this, duration: Duration(milliseconds: 200)),
+    );
+    _messages.insert(0, newMessage);
+    newMessage.animationController.forward();
+    setState(() {
+      this._isTyping = false;
+    });
+    _textController.clear();
+    _focusNode.requestFocus();
+  }
+
+  @override
+  void dispose() {
+    // TODO: Off del socket
+    for(ChatMessageWidget message in _messages)
+    {
+      message.animationController.dispose();
+    }
+    super.dispose();
   }
 }
---
+++ lib/src/widgets/chat_message_widget.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class ChatMessageWidget extends StatelessWidget {
+  //const ChatMessageWidget({Key? key}) : super(key: key);
+
+  final String text;
+  final String uid;
+  final AnimationController animationController;
+
+  const ChatMessageWidget({
+    Key? key, 
+    required this.text, 
+    required this.uid, 
+    required this.animationController
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: this.animationController,
+      child: SizeTransition(
+        sizeFactor: CurvedAnimation(
+          parent: this.animationController, 
+          curve: Curves.easeOut
+        ),
+        child: Container(
+          child: this.uid == '123'
+          ? _myMessage()
+          : _notMyMessage(),
+        ),
+      ),
+    );
+  }
+
+  Widget _myMessage(){
+    return Align(
+      alignment: Alignment.centerRight,
+      child: Container(
+        padding: EdgeInsets.all(8.0),
+        margin: EdgeInsets.only(bottom: 5.0, left: 50.0, right: 5.0),
+        child: Text(this.text, style: TextStyle(color: Colors.white),),
+        decoration: BoxDecoration(
+          color: Color(0xff4D9EF6),
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+      ),
+    );
+  }
+
+  Widget _notMyMessage(){
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Container(
+        padding: EdgeInsets.all(8.0),
+        margin: EdgeInsets.only(bottom: 5.0, left: 5.0, right: 50.0),
+        child: Text(this.text, style: TextStyle(color: Colors.black87),),
+        decoration: BoxDecoration(
+          color: Color(0xffE4E5E8),
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+      ),
+    );
+  }
+
+}
